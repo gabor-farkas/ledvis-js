d1ed609a6dac5cb2451000fa311d00c1a1ec0482
gabor-farkas <gabor.farkas@doctusoft.com>
2018-01-18 17:02:03 +0100

    kukac game working

--- effects/kukac/kukac.js
+++ effects/kukac/kukac.js
@@ -1,16 +1,118 @@
-function kukacEffect() {
+function kukacEffect(context) {
+    const initiallen = 4;
+    const maxlength = 64;
+    // the head is always the 0 index
+    let pixels = [];
+    let foodPos = [0, 0];
+    let direction = [1, 0];
+    const possibleDirections = [
+        [0, -1], [1, 0], [0, 1], [-1, 0]
+    ];
+    restartGame = function () {
+        pixels = [];
+        for (let i = 0; i < initiallen; i++) {
+            pixels.push([12, 12 + i]);
+        }
+        placeFood();
+        findDirection();
+    }
+    placeFood = function () {
+        let placed = false;
+        do {
+            foodPos = [random(23), random(23)];
+        } while (doesCollide(foodPos));
+    }
+    vectorAdd = function (v1, v2) {
+        let result = [];
+        for (let i = 0; i < v1.length; i++) {
+            result.push(v1[i] + v2[i]);
+        }
+        return result;
+    }
+    vectorCompare = function (v1, v2) {
+        for (let i = 0; i < v1.length; i++) {
+            if (v1[i] != v2[i])
+                return false;
+        }
+        return true;
+    }
+    findDirection = function () {
+        let head = pixels[0];
+        if (head[1] < foodPos[1]) {
+            direction = [0, 1];
+        } else if (head[1] > foodPos[1]) {
+            direction = [0, -1];
+        } else {
+            if (head[0] < foodPos[0]) {
+                direction = [1, 0];
+            } else {
+                direction = [-1, 0];
+            }
+        }
+        if (!doesCollide(vectorAdd(head, direction)))
+            return;
+        // find another direction
+        let foundDirection = null;
+        possibleDirections.forEach(candidateDirection => {
+            if (!doesCollide(vectorAdd(head, candidateDirection))) {
+                foundDirection = candidateDirection;
+                return false;
+            }
+        });
+        
+        if (foundDirection) {
+            direction = foundDirection;
+        } else {
+            // we died, there's no possible direction
+            restartGame();
+        }
+    }
+    doesCollide = function (vector) {
+        if (vector[0] < 0 || vector[0] > 23)
+            return true;
+        if (vector[1] < 0 || vector[1] > 23)
+            return true;
+        let result = false;
+        pixels.forEach(pixel => {
+            if (vectorCompare(pixel, vector))
+                result = true;
+        });
+        return result;
+    }
+    kukacStep = function () {
+        let head = pixels[0];
+        let newHead = vectorAdd(head, direction);
+        pixels.unshift(newHead);
+        if (!vectorCompare(newHead, foodPos)) {
+            pixels.pop();
+        } else {
+            placeFood();
+        }
+        if (pixels.length == maxlength) {
+            restartGame();
+        } else {
+            findDirection();
+        }
+    }
+    kukacRender = function () {
+        for (let i = 0; i < 24 * 24; i++)
+            context.screen[i] = 0;
+        pixels.forEach(pixel => {
+            context.screen[pixel[0] + 24 * pixel[1]] = 128;
+        });
+        context.screen[foodPos[0] + 24 * foodPos[1]] = 255;
+    }
     return {
         initialize: () => {
-            randomize();
             restartGame();
         },
         destroy: () => {
         },
         step: () => {
-
+            kukacStep();
         },
         render: () => {
-            
+            kukacRender();
         }
     }
 }
